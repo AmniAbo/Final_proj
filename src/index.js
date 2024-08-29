21c2c46120c10137151668704f080ba4edd36f23
AmniAbo <157361452+AmniAbo@users.noreply.github.com>
2024-08-29 23:20:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css'; // Path is correct since index.css is in the same folder
 import App from './App'; // Path is correct since App.js is in the same folder
-//
+import Home from './Home'; // Import your Home component
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* You can add more routes here as needed */}
+        <Route path="/app" element={<App />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
